21b824637ddc569b75e4dc555893b0ae6e45cbc6
Ashrit Verma <ashrit.verma@duke.edu>
2020-10-15 18:24:46 -0400

    Final draft with JavaDoc w/o debug

--- src/HashListAutocomplete.java
+++ src/HashListAutocomplete.java
@@ -13,6 +13,13 @@ public class HashListAutocomplete implements Autocompletor {
         initialize(terms, weights);
     }
 
+    /**
+     * Returns a sublist of the first k entries with the corresponding value
+     * if the prefix is in the map
+     * @param prefix substring of Term's String component that is used as keys
+     * @param k number of entries being returned
+     * @return
+     */
     @Override
     public List<Term> topMatches(String prefix, int k) {
         if(myMap.containsKey(prefix.substring(MAX_PREFIX)))
@@ -23,6 +30,11 @@ public class HashListAutocomplete implements Autocompletor {
         return null;
     }
 
+    /**
+     * Initializes instance variables by call from constructor
+     * @param terms is array of Strings for words in each Term
+     * @param weights is corresponding weight for word in terms
+     */
     @Override
     public void initialize(String[] terms, double[] weights) {
         myTerms = new Term[terms.length];
@@ -32,10 +44,11 @@ public class HashListAutocomplete implements Autocompletor {
 
         //Arrays.sort(myTerms);
 
-        for(Term term : myTerms)
+        for(Term element : myTerms)
         {
-            myMap.putIfAbsent(term.getWord().substring(MAX_PREFIX), new ArrayList<Term>());
-            myMap.get(term).add(term);
+            if(!myMap.containsKey(element.getWord().substring(MAX_PREFIX)))
+                myMap.put(element.getWord().substring(MAX_PREFIX), new ArrayList<Term>());
+            myMap.get(element).add(element);
         }
 
         for(String element : myMap.keySet())
@@ -45,6 +58,13 @@ public class HashListAutocomplete implements Autocompletor {
         }
     }
 
+    /**
+     * Returns bytes taken up by each string which contributes
+     * byter_per_char * length and each double stored, thus
+     * accounting for every Term and for all String keys in the map
+     * @return size in bytes of memory by accounting for every Term object and
+     * calculating length of every key in the map
+     */
     @Override
     public int sizeInBytes() {
         if(mySize == 0)
